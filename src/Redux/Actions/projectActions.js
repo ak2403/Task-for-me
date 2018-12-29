c11271f2962bfb3e0160e3bc9fb801619c4f5b42
ak2403 <arunpasupathi24@gmail.com>
2018-12-30 00:29:59 +1100

    File fix

--- src/Redux/Actions/projectActions.js
+++ src/Redux/Actions/projectActions.js
@@ -3,14 +3,14 @@ import * as getAPI from '../../api/getAPI'
 import * as postAPI from '../../api/postAPI'
 import * as deleteAPI from '../../api/deleteAPI'
 
-export const getProjects = () => {
-    return async dispatch => {
-        let getResponse = await getAPI.getProjectsCall()
-    }
-}
+// export const getProjects = () => {
+//     return async dispatch => {
+//         let getResponse = await getAPI.getProjectsCall()
+//     }
+// }
 
-export const addProject = data => {
-    return async dispatch => {
-        let getResponse = await postAPI.addProjectCall(data)
-    }
-}
+// export const addProject = data => {
+//     return async dispatch => {
+//         let getResponse = await postAPI.addProjectCall(data)
+//     }
+// }
