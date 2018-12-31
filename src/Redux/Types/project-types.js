38d14377950bdcf750b1a0189bcb306a8848f848
ARUN KUMAR <arunpasupathi24@gmail.com>
2018-12-31 18:53:47 +1100

    Delete project-types.js

--- src/Redux/Types/project-types.js
+++
@@ -1,3 +0,0 @@
-export const GET_PROJECT = 'get the list of projects of the user'
-export const ADDED_PROJECT = 'added a new project by the user'
-export const RESET_SETTINGS = 'reset the default values'
