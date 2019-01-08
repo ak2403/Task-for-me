eb837d4f557dc3784c45bc0d0ab9a77d1fcd8813
ak2403 <arunpasupathi24@gmail.com>
2019-01-09 10:50:20 +1100

    Update in config

--- src/api/config.js
+++ src/api/config.js
@@ -3,7 +3,8 @@ import jwt from 'jsonwebtoken'
 const getToken = localStorage.getItem('authToken')
 const decodeToken = jwt.decode(getToken)
 
-export const API_URL = 'http://localhost:5000'
+// export const API_URL = 'http://localhost:5000'
+export const API_URL = 'https://stormy-stream-73100.herokuapp.com'
 export let userID = decodeToken ? decodeToken.id : ''
 
 export const Authorization_header = {
